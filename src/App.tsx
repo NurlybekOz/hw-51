3e6f4cccbbbc1f6ac0516b46e0e5018243b78ab8
Nurlybek <nurlybekoroz@gmail.com>
2024-12-18 12:58:09 +0600

    Исправил набор из пяти случайных чисел, сделал так чтобы не повторялись числа

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,10 @@ const App = () => {
   const copyBall4 = {...copyBalls[3]};
   const copyBall5 = {...copyBalls[4]};
   copyBall1.count = Math.floor(Math.random() * (5 - 1 + 1) + 1);
-  copyBall2.count = Math.floor(Math.random() * (11 - 5 + 1) + 5);
-    copyBall3.count = Math.floor(Math.random() * (16 - 11 + 1) + 11);
-    copyBall4.count = Math.floor(Math.random() * (23 - 16 + 1) + 16);
-    copyBall5.count = Math.floor(Math.random() * (32 - 23 + 1) + 23);
+  copyBall2.count = Math.floor(Math.random() * (11 - 6 + 1) + 6);
+    copyBall3.count = Math.floor(Math.random() * (16 - 12 + 1) + 12);
+    copyBall4.count = Math.floor(Math.random() * (23 - 17 + 1) + 17);
+    copyBall5.count = Math.floor(Math.random() * (32 - 24 + 1) + 24);
   copyBalls[0] = copyBall1;
     copyBalls[1] = copyBall2;
     copyBalls[2] = copyBall3;
